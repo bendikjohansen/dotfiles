094ed1b62e340993481d2739fd0be4893a6e3770
Bendik Johansen <bendikj@gmail.com>
2025-11-14 15:32:06 +0100

    Clean up and install some languages by default

--- mac/setup.sh
+++ mac/setup.sh
@@ -3,5 +3,10 @@
 ## Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew bundle install
+mise use -g node
+mise use -g dotnet
+mise use -g java@21
+
 ## Stow dotfiles
 stow -R --dotfiles --ignore=.DS_Store --target ~ --dir=config home lazydocker lazygit nvim
